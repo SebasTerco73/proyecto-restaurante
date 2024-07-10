58321deec6d12bd6d4249b7429b9f0c239c9cb93
Mateo J. Sosa <127888586+Kabusnigo@users.noreply.github.com>
2024-07-09 23:05:23 -0300

    Update dataBase.js

--- db/dataBase.js
+++ db/dataBase.js
@@ -2,17 +2,18 @@ const mySql = require('mysql2');
 
 const connection = mySql.createConnection(
     {
+        /*
         host: 'localhost',
         user: 'root',
         password : '',
         database: 'restaurante'
+        */
         
-        /*
         host: DB_HOST,
         user: DB_USER,
         password : DB_PASSWORD,
         database: DB_NAME
-        */
+        
     }
 );
 
